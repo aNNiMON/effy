3f441daa10dc86bb77f49798762a3aa8ea4a8110
aNNiMON <annimon119@gmail.com>
2025-08-02 22:12:58 +0300

    Add pane selecting and Info pane text scrolling

--- src/app.rs
+++ src/app.rs
@@ -2,14 +2,40 @@ use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{DefaultTerminal, Frame};
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Pane {
+    Info,
+    Params,
+    Config,
+}
+
 pub struct App {
     running: bool,
+    pub current_pane: Pane,
+    pub info_text: String,
+    pub info_pane_current_line: u16,
 }
 
 impl App {
     pub fn new() -> Self {
-        Self::default()
+        App {
+            running: false,
+            current_pane: Pane::Info,
+            info_text: "Input file: abc.mp4\n\
+                 File size: 1.1MiB\n\
+                 Duration: 05:16\n\
+                 Has Audio: Yes\n\
+                 Has Video: Yes\n\
+                 Video Codec: h264\n\
+                 Audio Codec: aac\n\
+                 Video Resolution: 1920x1080\n\
+                 Video Bitrate: 4500kbps\n\
+                 Audio Bitrate: 192kbps\n\
+                 Video FPS: 30\n\
+                 Audio Channels: 2"
+                .to_string(),
+            info_pane_current_line: 0,
+        }
     }
 
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
@@ -34,11 +60,32 @@ impl App {
     }
 
     fn on_key_event(&mut self, key: KeyEvent) {
-        match (key.modifiers, key.code) {
-            (_, KeyCode::Esc | KeyCode::Char('q'))
-            | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
+        match (self.current_pane, key.modifiers, key.code) {
+            (_, _, KeyCode::Esc | KeyCode::Char('q'))
+            | (_, KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
+            (_, _, KeyCode::Tab) => self.next_pane(),
+            (Pane::Info, _, KeyCode::Down | KeyCode::Char('j')) => self.scroll_info_pane_down(),
+            (Pane::Info, _, KeyCode::Up | KeyCode::Char('k')) => self.scroll_info_pane_up(),
             _ => {}
         }
+    }
+
+    fn scroll_info_pane_down(&mut self) {
+        if self.info_pane_current_line < self.info_text.lines().count() as u16 - 1 {
+            self.info_pane_current_line += 1;
+        }
+    }
+
+    fn scroll_info_pane_up(&mut self) {
+        self.info_pane_current_line = self.info_pane_current_line.saturating_sub(1);
+    }
+
+    fn next_pane(&mut self) {
+        self.current_pane = match self.current_pane {
+            Pane::Info => Pane::Params,
+            Pane::Params => Pane::Config,
+            Pane::Config => Pane::Info,
+        };
     }
 
     fn quit(&mut self) {
--- src/ui.rs
+++ src/ui.rs
@@ -1,21 +1,77 @@
 use ratatui::{
-    style::Stylize,
+    layout::{Constraint, Direction, Layout},
+    style::{Style, Stylize},
     text::Line,
-    widgets::{Block, Paragraph, Widget},
+    widgets::{Block, Borders, Paragraph, Widget},
 };
 
-use crate::app::App;
+use crate::app::{App, Pane};
 
 impl Widget for &App {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
     {
-        let title = Line::from(" effy ").bold().blue().centered();
-        let text = "Input file: abc.mp4\n\
-            File size: 1.1MiB\n\
-            Duration: 05:16";
-        let paragraph = Paragraph::new(text).block(Block::bordered().title(title));
-        paragraph.render(area, buf);
+        let default_style = Style::new().white();
+        let highlighted_style = default_style.blue();
+
+        let [info, main] = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Max(5), Constraint::Fill(1)])
+            .areas(area);
+        {
+            let style = if matches!(self.current_pane, Pane::Info) {
+                highlighted_style
+            } else {
+                default_style
+            };
+            Paragraph::new(self.info_text.clone())
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .border_style(style)
+                        .title_top(Line::from(" effy ").bold().blue().centered())
+                        .title_top(Line::from(" Info ").blue().left_aligned()),
+                )
+                .scroll((self.info_pane_current_line, 0))
+                .render(info, buf);
+        }
+
+        let [params, config] = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Fill(1), Constraint::Fill(3)])
+            .areas(main);
+        {
+            let style = if matches!(self.current_pane, Pane::Params) {
+                highlighted_style
+            } else {
+                default_style
+            };
+            Paragraph::new("Params")
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .border_style(style)
+                        .title_top(Line::from(" Params ").blue().left_aligned()),
+                )
+                .render(params, buf);
+        }
+
+        {
+            let style = if matches!(self.current_pane, Pane::Config) {
+                highlighted_style
+            } else {
+                default_style
+            };
+            Paragraph::new("Config area")
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .border_style(style)
+                        .title_top(Line::from(" Config ").blue().left_aligned())
+                        .title_bottom(Line::from(" Press 'q' to quit ").dim().left_aligned()),
+                )
+                .render(config, buf);
+        }
     }
 }
